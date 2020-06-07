7ea5d7c5e6f70d810564bbaadc5f2558bf8b225a
snehaKums <43563928+snehaKums@users.noreply.github.com>
2020-06-08 00:15:33 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,26 @@
 import React,{Component} from 'react';
-import {View,Text,} from 'react-native';
-import {Button,Input} from 'react-native-elements'; 
+import {View,Text,TextInput,TouchableOpacity} from 'react-native';
 import firebase from 'firebase';
 import styles from './styles';
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
+  apiKey: "AIzaSyDhQNGL8823UPpKWCk1jUyqekthkS8rVg0",
   authDomain: "newproj-93dfe.firebaseapp.com",
   databaseURL: "https://newproj-93dfe.firebaseio.com",
   projectId: "newproj-93dfe",
   storageBucket: "newproj-93dfe.appspot.com",
-  messagingSenderId: "ID",
-  appId: "APP_ID,
+  messagingSenderId: "397006982875",
+  appId: "1:397006982875:web:0599157e6f3221adb506fe",
+  measurementId: "G-YPWGLT6XV2"
 };
 firebase.initializeApp(firebaseConfig);
 
 export default class Login extends Component{
+  static navigationOptions = ({navigation}) => {
+    return{
+      header: null
+   }
+  }
   constructor(props)
 {
   super(props);
@@ -43,25 +48,37 @@ render(){
         
         <Text style={styles.header}>Welcome !!!</Text>
         <Text style={styles.subHeader}> Login to continue</Text>
-        <Input 
-        placeholder= "Enter Name"
-        onChangeText = {(name) => this.setState({name})}
-        />
-        <Input 
-        placeholder = " Enter Email"
-        onChangeText = {(email) => this.setState({email})}
-        />
-         <Input 
-        placeholder = "Password"
-        onChangeText = {(password) => this.setState({password})}
+
+        <Text style={styles.placeholderStyle}>Enter Name</Text>
+        <TextInput 
+        style={styles.textInput}
+        onChangeText={name => this.setState({ name })}
+        value={this.state.name}
         />
 
-        <View>
-          <Button 
-          title=" Login In"
-          onPress = {() => this.loginIn(this.state.name,this.state.email,this.state.password)}
-          />
-        </View>
+        <Text style={styles.placeholderStyle}>Email</Text>
+        <TextInput 
+        style={styles.textInput}
+        onChangeText={email => this.setState({ email })}
+        value={this.state.email}
+        />
+
+        <Text style={styles.placeholderStyle}>Password</Text>
+        <TextInput 
+        style={styles.textInput}
+        onChangeText={password => this.setState({ password })}
+        value={this.state.password}
+        />
+
+        <TouchableOpacity style={styles.buttonStyle}  
+            onPress = {() => this.loginIn(this.state.name,this.state.email,this.state.password)}>
+          <Text style={styles.buttonText}>Login In</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonStyle}  
+            onPress={() => this.props.navigation.navigate('SignUp')}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
       </View>
     )
   }
